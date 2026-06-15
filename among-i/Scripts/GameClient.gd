6313502b76c03d11caf40908bf0a3f4214d8247d
Allen-Mikhailov <mikhailovallen@gmail.com>
2026-06-15 02:42:12 -0500

    Many small changes made. Refactored.

--- among-i/Scripts/GameClient.gd
+++ among-i/Scripts/GameClient.gd
@@ -5,7 +5,7 @@ var id: int
 var socket: WebSocketPeer # Or PacketPeerUDP / WebSocketPeer
 var node: Node       # Replace with your actual Player script type
 var name: String = "undefined"
-var color_index: int
+var index: int
 var first_time: bool = true
 var chat_context: Array = []
 var tile: Vector2i
@@ -16,9 +16,8 @@ var is_active: bool = false
 var wipe_next_update: bool = false
 
 # A constructor to make creating them easy
-func _init(_id: int, _socket, _node, _color: int):
+func _init(_id: int, _node, _index: int):
 	id = _id
-	socket = _socket
 	node = _node
-	color_index = _color
+	index = _index
 	is_active = false
